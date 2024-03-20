beffdc26795c6a01d88a19a91c92ca218482c8ab
Success <121008519+Success05081988@users.noreply.github.com>
2024-03-21 03:41:12 +0500

    Update base-mbr.sh

--- base-mbr.sh
+++ base-mbr.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "<<------ START INSTALL ARCH-LINUX ------>>"
-sleep 3
 pacman -Syy
 ln -sf /usr/share/zoneinfo/Asia/Tashkent /etc/localtime
 hwclock --systohc
-sed -i '398s/.//' /etc/locale.gen
+sed -i '171s/.//' /etc/locale.gen
+sed -i '400s/.//' /etc/locale.gen
 locale-gen
-sleep 3
 echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=ru" >> /etc/vconsole.conf
 echo "FONT=cyr-sun16" >> /etc/vconsole.conf
@@ -14,57 +13,45 @@ echo "arch" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-sleep 3
-echo root:password | chpasswd
 
-pacman -S --noconfirm linux-lts-headers sof-firmware grub alsa-utils \
-efibootmgr networkmanager network-manager-applet dialog \
-wpa_supplicant mtools dosfstools reflector bash-completion \
-openssh rsync acpi acpi_call-lts tlp acpid ntfs-3g \
-terminus-font ttf-terminus-nerd ttf-roboto man fwupd git os-prober
-#for printers and bluetoth (bluez bluez-utils cups hplip)
+pacman -S --noconfirm linux-lts-headers sof-firmware grub alsa-utils alsa-oss \
+alsa-firmware alsa-card-profiles alsa-plugins efibootmgr networkmanager \
+network-manager-applet dialog wpa_supplicant mtools dosfstools \
+bash-completion openssh rsync acpi acpi_call-lts \
+gst-plugins-bad gst-plugins-ugly gst-plugins-base gst-plugins-good gst-plugin-opencv gst-plugins-espeak \
+gst-plugin-pipewire gst-plugins-bad-libs gst-plugin-libcamera gst-plugins-base-libs \
+acpid ntfs-3g terminus-font ttf-terminus-nerd ttf-roboto man fwupd git
 
-sleep 3
 
 pacman -S --noconfirm xorg
 
-sleep 3
 
 pacman -S --noconfirm plasma 
 
-sleep 5
 
-pacman -S --noconfirm egl-wayland konsole dolphin plasma-wayland-session \
-neofetch htop inxi bpytop elisa vlc chromium \
-okular gwenview ark vim vim-plugins partitionmanager \
-spectacle telegram-desktop clipgrab 
+pacman -S --noconfirm konsole dolphin neofetch htop inxi bpytop elisa chromium okular gwenview obs-studio
 
-sleep 5
 
-grub-install --target=i386-pc /dev/sda
-#grub-install --target=i386-pc /dev/sda # replace sdx with your disk name, not the partition
+#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=VIRT
+grub-install --target=i386-pc /dev/vda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-sleep 3
 
 systemctl enable NetworkManager
 #systemctl enable bluetooth
 #systemctl enable cups.service
-systemctl enable sshd
+#systemctl enable sshd
 #systemctl enable avahi-daemon
 systemctl enable sddm 
-systemctl enable reflector.timer
+#systemctl enable reflector.timer
 systemctl enable fstrim.timer
 systemctl enable acpid
 
-sleep 3
 
-useradd -m orif1988
-echo orif1988:password | chpasswd
-echo "orif1988 ALL=(ALL) ALL" >> /etc/sudoers.d/orif1988
-usermod -c 'Orif Ismailov' orif1988
+useradd -m virt
+echo "virt ALL=(ALL) ALL" >> /etc/sudoers.d/virt
+#usermod -c 'Orif Ismailov' virt
 
-sleep 3
 
 printf "\e[1;32mDone! Type exit, umount -a /mnt and reboot.\e[0m"
 
